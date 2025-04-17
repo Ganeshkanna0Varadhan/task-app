844d91f564822773f540e993a166154d8fbfe507
ganeshkanna_varadhan <ganeshkanna718@gmail.com>
2025-04-17 20:02:52 +0530

    update login page

--- src/login/login.component.ts
+++ src/login/login.component.ts
@@ -1,12 +1,33 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  showPassword: boolean = false;
+  ngForm!: FormGroup;
 
+  ngOnInit(): void {
+    this.ngForm = new FormGroup({
+      gmail: new FormControl("", Validators.required),
+      password: new FormControl("", [Validators.required, Validators.minLength(8)])
+      
+    })
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    
+  }
+
+  togglePassword() {
+    this.showPassword = !this.showPassword;
+  }
 }
